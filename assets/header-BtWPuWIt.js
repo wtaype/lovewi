befc0f7a4992b791427c1b525588f39ff3a8ab77
wtaype <wtaype@users.noreply.github.com>
2026-03-15 23:03:43 +0000

    deploy: f10e0e45592990f7ade4113f7c091d94d59611a6

---
+++ assets/header-BtWPuWIt.js
@@ -0,0 +1,11 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/login-B96lPrB5.js","assets/firebase-SpmmtACv.js","assets/wii-a5uC_Ctk.js","assets/firebase-C8K7HU58.js","assets/widev-CNgclzEO.js","assets/ruta-8ukjGcg_.js","assets/preload-helper-CPshegQA.js","assets/login-pzxUIRzl.css"])))=>i.map(i=>d[i]);
+import{_ as n}from"./preload-helper-CPshegQA.js";import{rutas as l}from"./ruta-8ukjGcg_.js";import{M as c,$ as s,a as r}from"./widev-CNgclzEO.js";import"./wii-a5uC_Ctk.js";const e=a=>{c?.("Bienvenido "+a.nombre),s(".nv_right").html(`
+    <a href="/milab" class="nv_item" data-page="milab"><i class="fa-solid fa-graduation-cap"></i> <span>Mi Lab</span></a>
+    <a href="/smile" class="nv_item" data-page="smile"><i class="fa-solid fa-comments"></i> <span>Mensajes</span></a>
+    <a href="/perfil" class="nv_item" data-page="perfil"><img src="${a.imagen||"./smile.avif"}" alt="${a.nombre}"><span>${a.nombre}</span></a>
+    <button class="nv_item bt_salir" data-page="inicio"><i class="fa-solid fa-sign-out-alt"></i> <span>salir</span></button>
+  `)},o=()=>{s(".nv_right").html(`
+    <a href="/descubre" class="nv_item" data-page="descubre"><i class="fa-solid fa-gauge"></i> <span>Descubre </span></a>
+    <button class="bt_auth registrar"><i class="fas fa-user-plus"></i><span>Registrar</span></button>
+    <button class="bt_auth login"><i class="fas fa-sign-in-alt"></i><span>Login</span></button>  
+  `)};r.on(a=>a?e(a):(o(),l.navigate("/")));const t=r.user;t?e(t):o();const m=["wiTema","wiSmart","wiFresh"];s(document).on("click",".bt_salir",async()=>{const{salir:a}=await n(async()=>{const{salir:i}=await import("./login-B96lPrB5.js");return{salir:i}},__vite__mapDeps([0,1,2,3,4,5,6,7]));a(m)});s(document).on("click",".wibtn_auth",async function(){const{abrirLogin:a}=await n(async()=>{const{abrirLogin:i}=await import("./login-B96lPrB5.js");return{abrirLogin:i}},__vite__mapDeps([0,1,2,3,4,5,6,7]));a(s(this).hasClass("registrar")?"registrar":"login")});export{e as personal};
---
+++ assets/wii-a5uC_Ctk.js
@@ -0,0 +1,1 @@
+let e="lovewi",t="Lovewi",a=2026,i="@wilder.taype",l,s="https://wtaype.github.io/",o="v16";export{t as a,l as b,i as c,a as d,e as i,s as l,o as v};
